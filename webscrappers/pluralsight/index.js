2eee521f64a2f01347bb59ef70b6fab363d186c0
Zakpak0 <ZakharyOliver808@gmail.com>
2021-11-25 21:51:46 -1000

    update

--- webscrappers/pluralsight/index.js
+++ webscrappers/pluralsight/index.js
@@ -1,22 +1,58 @@
 import https from "https";
-import url from "url";
-import { Buffer } from "buffer";
-import fs from "fs";
-const getPluralsightInfo = () => {
-  let body;
-  https.get(
-    "https://app.pluralsight.com/profile/zakhary-oliver",
-    (response) => {
-      response.setEncoding("utf-8");
-      response.on("data", (data) => {
-        body += data;
-      });
-      response.on("close", (join) => {
-        fs.writeFileSync("./pluralsightContent.js", body, (err) => {
-          console.log(err);
+export const getPluralsightInfo = {
+  currentlyLearning: (callback) =>
+    https.get(
+      "https://app.pluralsight.com/profile/data/currentlylearning/83b81959-5219-4864-a1f6-00bfa47c976f",
+      (response) => {
+        let body = "";
+        response.on("data", (data) => {
+          body += data;
         });
-      });
-    }
-  );
+        response.on("close", (join) => {
+          body = JSON.parse(body);
+          return callback(body);
+        });
+      }
+    ),
+  badges: (callback) =>
+    https.get(
+      "https://app.pluralsight.com/profile/data/badges/83b81959-5219-4864-a1f6-00bfa47c976f",
+      (response) => {
+        let body = "";
+        response.on("data", (data) => {
+          body += data;
+        });
+        response.on("close", (join) => {
+          body = JSON.parse(body);
+          return callback(body);
+        });
+      }
+    ),
+  completedCoures: (callback) =>
+    https.get(
+      "https://app.pluralsight.com/profile/data/completedcourses/83b81959-5219-4864-a1f6-00bfa47c976f",
+      (response) => {
+        let body = "";
+        response.on("data", (data) => {
+          body += data;
+        });
+        response.on("close", (join) => {
+          body = JSON.parse(body);
+          return callback(body);
+        });
+      }
+    ),
 };
-getPluralsightInfo();
+export const currentlyLearning = getPluralsightInfo.currentlyLearning(
+  (callback) => {
+    return callback;
+  }
+);
+export const badges = getPluralsightInfo.badges((callback) => {
+  return callback;
+});
+export const completedCoures = getPluralsightInfo.completedCoures(
+  (callback) => {
+    return callback;
+  }
+);
