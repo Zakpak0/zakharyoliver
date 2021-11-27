bb7bc4d26d52c659cb30c16fd13507e12566d5d3
LAPTOP-KO3E8U1N\18082 <ZakharyOliver808@gmail.com>
2021-11-27 09:53:48 -1000

    Create stitches.config.js

---
+++ stitches.config.js
@@ -0,0 +1,42 @@
+import { createStitches } from '@stitches/react';
+import {
+    gray,
+    blue,
+    red,
+    green,
+    grayDark,
+    blueDark,
+    redDark,
+    greenDark,
+  } from '@radix-ui/colors';
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {
+      ...gray,
+      ...blue,
+      ...red,
+      ...green,
+    },
+  },
+  darkTheme: {
+    ...grayDark,
+    ...blueDark,
+    ...redDark,
+    ...greenDark,
+  },
+  media: {
+    bp1: '(min-width: 480px)',
+  },
+  utils: {
+    marginX: (value) => ({ marginLeft: value, marginRight: value }),
+  },
+});
